117b1ec663712cd15652f5f5d97e79159c9d11ea
Bruteda <anders.e.khans@gmail.com>
2020-04-07 12:58:16 +0200

    cors

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app = express()
 const port = 3000
 const path = require('path');
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    return next();
+
+});
+
+
 app.get('/shops/', (req, res) => {
 
     res.sendFile(path.join(__dirname, '', 'shops.json'));
